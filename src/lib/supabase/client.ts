978a87699284f2275ad7dbb813183dfae488b629
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2026-06-18 09:43:06 +0530

    created browser and server supabase client

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,6 @@
 // src/lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
-
+import { createBrowserClient } from '@supabase/ssr';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
