168d01ae90e4f76f1ca2d8b932b49990eb51a6af
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2026-06-18 15:10:00 +0530

    Refactor Supabase client initialization into createClient function

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,4 +3,8 @@ import { createBrowserClient } from '@supabase/ssr';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
 
-export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
+
+
+export function createClient() {
+  return createBrowserClient(supabaseUrl, supabaseKey);
+}
