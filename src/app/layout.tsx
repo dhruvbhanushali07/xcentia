4011ff328fa49e0ab3ec64c6790a40dc0e227ab0
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2026-06-18 12:32:53 +0530

    Implement user authentication in RootLayout by fetching current user from Supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,36 +6,48 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BagStoreProvider } from "@/providers/Bag-store-provider";
-
+import { createClient } from "@/lib/supabase/server";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Scentwale | Premium Fragrances",
-  description: "Discover your signature scent with Scentwale.",
+	title: "Scentwale | Premium Fragrances",
+	description: "Discover your signature scent with Scentwale.",
 };
 
-export default function RootLayout({
-  children,
+export default async function RootLayout({
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      
-      <body className={`${inter.className} bg-gray-50 text-gray-900`}>
-        {/* Navigation Bar */}
-        <BagStoreProvider>
-        <Navbar />
 
-        {/* Dynamic Page Content goes here */}
-        <main className="min-h-screen">
-          {children}
-        </main>
+  const supabase= await createClient()
 
-        {/* Footer */}
-        <Footer />
-        </BagStoreProvider>
-      </body>
-    </html>
-  );
+  async function getCurrentUser(){
+    const {data,error}= await supabase.auth.getUser()
+    if(!error){
+      return data
+    }
+    console.log(error)
+    return null
+  }
+
+  const currentUser= await getCurrentUser()
+  console.log(currentUser)
+  
+	return (
+		<html lang="en">
+			<body className={`${inter.className} bg-gray-50 text-gray-900`}>
+				{/* Navigation Bar */}
+				<BagStoreProvider>
+					<Navbar user={currentUser?.user ?? null} />
+
+					{/* Dynamic Page Content goes here */}
+					<main className="min-h-screen">{children}</main>
+
+					{/* Footer */}
+					<Footer />
+				</BagStoreProvider>
+			</body>
+		</html>
+	);
 }
